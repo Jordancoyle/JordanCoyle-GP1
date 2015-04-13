2b56463f10fa3355e2485cba91ddf92be41fd88d
Jordan Coyle <engineerofsoftware@gmail.com>
2015-04-13 03:26:54 +0100

    Began work on both audio and scene change

--- JordanCoyle_GP1/JordanCoyle_GP1/main.cpp
+++ JordanCoyle_GP1/JordanCoyle_GP1/main.cpp
@@ -12,16 +12,20 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevHInstance, LPSTR cmdLine, 
 		screenHeight = 720,
 		windowBPP = 16;
 
-	cTexture backGround;
+	cTexture introTex,
+		backGroundTex;
 
-	cBkGround backGroundSprite;
+	cBkGround introSprite,
+		backGroundSprite;
 
 	windowOGL oglWindow;
 
 	static cWNDManager* windowMng = cWNDManager::getInstance();
 	static cSoundMgr* soundMng = cSoundMgr::getInstance();
+	static cInputMgr* inputMng = cInputMgr::getInstance();
 
 	windowMng->attachOGLWnd(&oglWindow);
+	windowMng->attachInputMgr(inputMng);
 
 	if (!windowMng->createWND(screenWidth, screenHeight, windowBPP))
 	{
@@ -37,12 +41,23 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevHInstance, LPSTR cmdLine, 
 		return 1;
 	}
 
-	backGround.createTexture("Images\\intro_screen.png");
-	backGroundSprite.setSpritePos(glm::vec2(0.0f, 0.0f));
-	backGroundSprite.setTexture(backGround.getTexture());
-	backGroundSprite.setTextureDimensions(backGround.getTWidth(), backGround.getTHeight());
+	inputMng->clearBuffers(inputMng->KEYS_DOWN_BUFFER | inputMng->KEYS_PRESSED_BUFFER);
 
-	backGroundSprite.attachSoundMgr(soundMng);
+	LPCSTR gameSounds[1] = { "Audio\\Space_Coast.mp3" };
+
+	soundMng->add("MenuMusic", gameSounds[0]);
+
+	introTex.createTexture("Images\\intro_screen.png");
+	introSprite.setSpritePos(glm::vec2(0.0f, 0.0f));
+	introSprite.setTexture(introTex.getTexture());
+	introSprite.setTextureDimensions(introTex.getTWidth(), introTex.getTHeight());
+
+	backGroundTex.createTexture("Images\\game_background.png");
+	backGroundSprite.setSpritePos(glm::vec2(0.0f, 0.0f));
+	backGroundSprite.setTexture(backGroundTex.getTexture());
+	backGroundSprite.setTextureDimensions(backGroundTex.getTWidth(), backGroundTex.getTHeight());
+
+	introSprite.attachSoundMgr(soundMng);
 
 	while (windowMng->isWNDRunning())
 	{
@@ -50,10 +65,27 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevHInstance, LPSTR cmdLine, 
 
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		backGroundSprite.render();
+		introSprite.render();
+
+		if (inputMng->anyKeyPressed() == true)
+		{
+			//windowMng->processWNDEvents();
+
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	
+			//introSprite.setActive(false);
+			backGroundSprite.render();
+
+			windowMng->swapBuffers();
+			//inputMng->clearBuffers(inputMng->KEYS_DOWN_BUFFER | inputMng->KEYS_PRESSED_BUFFER);
+		}
 
 		windowMng->swapBuffers();
+		inputMng->clearBuffers(inputMng->KEYS_DOWN_BUFFER | inputMng->KEYS_PRESSED_BUFFER);
 	}
+
+	oglWindow.shutdown();
+	windowMng->destroyWND();
 
 	return 0;
 }
